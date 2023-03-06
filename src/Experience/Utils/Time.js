529d502ff786663451f1e503356401cbcd722464
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-07 01:29:08 +0800

    added superclass

--- src/Experience/Utils/Time.js
+++ src/Experience/Utils/Time.js
@@ -1,5 +1,8 @@
-export default class Time {
+import EventEmitter from "events";
+
+export default class Time extends EventEmitter {
   constructor() {
+    super();
     this.start = Date.now();
     this.current = this.start;
     this.elapsed = 0;
@@ -11,9 +14,10 @@ export default class Time {
   update() {
     const currentTime = Date.now();
     this.delta = currentTime - this.current;
-
     this.current = currentTime;
     this.elapsed = this.current - this.start;
+    console.log(this.delta);
+    this.emit("update");
 
     window.requestAnimationFrame(() => this.update());
   }
