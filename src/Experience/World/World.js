6b8e8de5a43bbb49755eb65d05a5b3992be2c0c0
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-07 20:59:49 +0800

    upload resourses function connected

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -9,6 +9,13 @@ export default class World {
     this.scene = this.experience.scene;
     this.canvas = this.experience.canvas;
     this.camera = this.experience.camera;
+    this.resources = this.experience.resources;
+
+    this.resources.on("ready", () => {
+      this.room = new Room();
+      console.log("created room");
+    });
+
     this.room = new Room();
   }
 
