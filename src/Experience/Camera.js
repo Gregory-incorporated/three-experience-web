9642af323b0c0f3129989144fec20e2651734230
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-01 14:10:23 +0300

    added orbit controls

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -1,5 +1,7 @@
-import Experience from "./Experience";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import Experience from "./Experience";
+
 export default class Camera {
   constructor() {
     this.experience = new Experience();
@@ -9,6 +11,13 @@ export default class Camera {
 
     this.createPerspectiveCamera();
     this.createOrthographicCamera();
+    this.setOrbitControls();
+  }
+
+  setOrbitControls() {
+    this.controls = new OrbitControls(this.perspectiveCamera, this.canvas);
+    this.controls.enableDamping = true;
+    this.controls.enableZoom = true;
   }
 
   createPerspectiveCamera() {
@@ -24,17 +33,33 @@ export default class Camera {
   }
 
   createOrthographicCamera() {
-    this.frustrum = 5;
     this.orthographicCamera = new THREE.OrthographicCamera(
       (-this.sizes.aspect * this.sizes.frustrum) / 2,
       (this.sizes.aspect * this.sizes.frustrum) / 2,
       this.sizes.frustrum / 2,
       -this.sizes.frustrum / 2,
-      -100,
-      100
+      -50,
+      50
     );
 
+    // 6.5
+    this.orthographicCamera.position.y = 5.65;
+    this.orthographicCamera.position.z = 10;
+    this.orthographicCamera.rotation.x = -Math.PI / 6;
+
     this.scene.add(this.orthographicCamera);
+
+    // this.helper = new THREE.CameraHelper(this.orthographicCamera);
+    // this.scene.add(this.helper);
+
+    const size = 20;
+    const divisions = 20;
+
+    const gridHelper = new THREE.GridHelper(size, divisions);
+    this.scene.add(gridHelper);
+
+    const axesHelper = new THREE.AxesHelper(10);
+    this.scene.add(axesHelper);
   }
 
   resize() {
@@ -52,5 +77,7 @@ export default class Camera {
     this.orthographicCamera.bottom = -this.sizes.frustrum / 2;
   }
 
-  update() {}
+  update() {
+    this.controls.update();
+  }
 }
